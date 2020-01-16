b864934acf4bd1d4cb247e15764916b5fbf0f2f5
Dennis Schad <dennis.schad1991@gmail.com>
2020-01-16 11:14:07 +0100

    changed wording

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -5,7 +5,7 @@ const NotFoundPage = () => {
 		<div className="notFoundPage">
 			<div className="notFoundPage__textWrapper">
 				<div className="notFoundPage__statusCode">404</div>
-				<div className="notFoundPage__errorText">You're alone - <br />No Pokemon are present</div>
+				<div className="notFoundPage__errorText">You're alone - <br />No Pokémon are present</div>
 				<div className="notFoundPage__infoText">The requested URL is not available</div>
 			</div>
 		</div>
